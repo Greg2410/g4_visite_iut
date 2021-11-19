f3d1834a18e8b5df6cc6e9566bdb74078369a035
Greg2410 <84352988+Greg2410@users.noreply.github.com>
2021-11-19 13:06:00 +1100

    ajout du dynamisme changement d'image et infospot

--- main.js
+++ main.js
@@ -7,31 +7,232 @@
 //     infospot.addHoverText('');
 //   }
 
+const rayon = new PANOLENS.ImagePanorama('img/g4_int_i4.jpg').radius;
+
 const tableauImages = [
   {
+    'pano':  new PANOLENS.ImagePanorama('img/g4_int_i4.jpg'),
     'nom': 'g4_int_i4',
-     'rotation': 4.7,
       'infospot': [
-        {           
+        {
+          'phi' : Math.PI / 2,
+          'theta': 0,     
           'position' : new THREE.Vector3(
-            rayon * Math.cos( Math.PI / 7) * Math.sin(Math.PI / 1.7), // Attention Y sphère
-            rayon * Math.sin( Math.PI), // Attention Z de la sphère
-            rayon * Math.cos( Math.PI) * Math.cos((Math.PI / 1.7) / 1.2) // Attention X de la sphèrec
+            rayon * Math.cos(0) * Math.sin(Math.PI / 2), // Attention Y sphère
+            rayon * Math.sin(0), // Attention Z de la sphère
+            rayon * Math.cos(0) * Math.cos(Math.PI / 2) // Attention X de la sphère
           ),
           'text': 'sortie i4 couloir élève',
-            'rotation' :viewer.getControl().rotateLeft(1.5)
+          'destination': 'g4_couloir_i4'
         },
         { 
           'phi' : -0.7,
           'theta': Math.PI,    
           'position' : new THREE.Vector3(
-            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 1.7), // Attention Y sphère
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2 + Math.PI/8), // Attention Y sphère
             rayon * Math.sin(Math.PI), // Attention Z de la sphère
-            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 1.7) // Attention X de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2 + Math.PI/8) // Attention X de la sphère
           ),
           'text': 'sortie i4 couloir prof',
-            'rotation' :viewer.getControl().rotateLeft(4.7)
+          'destination': 'g4_couloir_prof_i4'
         } 
       ]
-    }
-  ];
+  },
+  {
+    'pano': new PANOLENS.ImagePanorama('img/g4_int_i6.jpg'),
+    'nom': 'g4_int_i6',
+      'infospot': [
+        {
+          'phi' : Math.PI / 2,
+          'theta': Math.PI,     
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2) // Attention X de la sphère
+          ),
+          'text': 'sortie i6 couloir élève',
+          'destination': 'g4_couloir_i6'
+        },
+        { 
+          'phi' : -0.7,
+          'theta': Math.PI,    
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(-14*Math.PI/15) * Math.sin(Math.PI / 2 + Math.PI), // Attention Y sphère
+            rayon * Math.sin(-14*Math.PI/15), // Attention Z de la sphère
+            rayon * Math.cos(-14*Math.PI/15) * Math.cos(Math.PI / 2 + Math.PI) // Attention X de la sphère
+          ),
+          'text': 'sortie i6 couloir prof',
+          'destination': 'g4_couloir_prof_i6'
+        } 
+      ]
+  },
+  {
+    'pano': new PANOLENS.ImagePanorama('img/g4_couloir_i4.jpg'),
+    'nom': 'g4_couloir_i4',
+      'infospot': [
+        {
+          'phi' : Math.PI / 2,
+          'theta': Math.PI,     
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2 + Math.PI/2), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2 + Math.PI/2) // Attention X de la sphère
+          ),
+          'text': 'salle i4 depuis couloir élève',
+          'destination': 'g4_int_i4'
+        },
+        { 
+          'phi' : Math.PI / 2,
+          'theta': 0,   
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2) // Attention X de la sphère
+          ),
+          'text': 'couloir i6 depuis couloir i4',
+          'destination': 'g4_couloir_i6'
+        },
+        { 
+          'phi' : Math.PI,
+          'theta': Math.PI,   
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(-14*Math.PI/15) * Math.sin(-Math.PI/6), // Attention Y sphère
+            rayon * Math.sin(-14*Math.PI/15), // Attention Z de la sphère
+            rayon * Math.cos(-14*Math.PI/15) * Math.cos(-Math.PI/6) // Attention X de la sphère
+          ),
+          'text': 'extérieur i4 depuis couloir i4',
+          'destination': 'g4_couloir_i4'
+        } 
+      ]
+  },
+  {
+    'pano':new PANOLENS.ImagePanorama('img/g4_couloir_i6.jpg'),
+    'nom': 'g4_couloir_i6',
+      'infospot': [
+        {
+          'phi' : Math.PI / 2,
+          'theta': Math.PI,     
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2*2), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2*2) // Attention X de la sphère
+          ),
+          'text': 'salle i6 depuis couloir élève',
+          'destination': 'g4_int_i6'
+        },
+        { 
+          'phi' : 0,
+          'theta': Math.PI,    
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(-15*Math.PI/16) * Math.sin(Math.PI / 2-Math.PI), // Attention Y sphère
+            rayon * Math.sin(-15*Math.PI/16), // Attention Z de la sphère
+            rayon * Math.cos(-15*Math.PI/16) * Math.cos(Math.PI / 2-Math.PI) // Attention X de la sphère
+          ),
+          'text': 'couloir élève i4 depuis couloir élève i6',
+          'destination': 'g4_couloir_i4'
+        },
+        { 
+          'phi' : 0,
+          'theta': Math.PI,    
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(-19*Math.PI/20) * Math.sin(Math.PI / 2), // Attention Y sphère
+            rayon * Math.sin(-19*Math.PI/20), // Attention Z de la sphère
+            rayon * Math.cos(-19*Math.PI/20) * Math.cos(Math.PI / 2) // Attention X de la sphère
+          ),
+          'text': 'couloir élève dbe depuis couloir élève i6',
+          'destination': 'g4_couloir_i6'
+        },
+        { 
+          'phi' : 0,
+          'theta': Math.PI,    
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(-15*Math.PI/16) * Math.sin(-Math.PI/4), // Attention Y sphère
+            rayon * Math.sin(-15*Math.PI/16), // Attention Z de la sphère
+            rayon * Math.cos(-15*Math.PI/16) * Math.cos(-Math.PI/4), // Attention X de la sphère
+          ),
+          'text': 'ext i4 depuis couloir élève i6',
+          'destination': 'g4_couloir_i6'
+        }
+      ]
+  },
+  {
+    'pano': new PANOLENS.ImagePanorama('img/g4_couloir_prof_i4.jpg'),
+    'nom': 'g4_couloir_prof_i4',
+     'rotation': 4.7,
+      'infospot': [
+        {
+          'phi' : Math.PI / 2,
+          'theta': Math.PI,     
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2-Math.PI), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2-Math.PI) // Attention X de la sphère
+          ),
+          'text': 'entrée i6 depuis couloir prof i4',
+          'destination': 'g4_couloir_prof_i6'
+        },
+        { 
+          'phi' : Math.PI / 2,
+          'theta': 0,   
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(-Math.PI), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(-Math.PI) // Attention X de la sphère
+          ),
+          'text': 'salle i4 depuis couloir prof i4',
+          'destination': 'g4_int_i4'
+        },
+        { 
+          'phi' : Math.PI,
+          'theta': Math.PI,   
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI / 2), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI / 2) // Attention X de la sphère
+          ),
+          'text': 'parking prof depuis couloir prof i4',
+          'destination': 'g4_couloir_prof_i4'
+        } 
+      ]
+  },
+  {
+    'pano': new PANOLENS.ImagePanorama('img/g4_couloir_prof_i6.jpg'),
+    'nom': 'g4_couloir_prof_i6',
+     'rotation': 4,
+      'infospot': [
+        {
+          'phi' : Math.PI / 2,
+          'theta': Math.PI,     
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(Math.PI), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(Math.PI) // Attention X de la sphère
+          ),
+          'text': 'salle i6 depuis couloir prof i6',
+          'destination': 'g4_int_i6'
+        },
+        { 
+          'phi' : Math.PI / 2,
+          'theta': 0,   
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(-19*Math.PI/20) * Math.sin(10*Math.PI/19), // Attention Y sphère
+            rayon * Math.sin(-19*Math.PI/20), // Attention Z de la sphère
+            rayon * Math.cos(-19*Math.PI/20) * Math.cos(10*Math.PI/19) // Attention X de la sphère
+          ),
+          'text': 'couloir prof i4 depuis couloir prof i6',
+          'destination': 'g4_couloir_prof_i4'
+        },
+        { 
+          'phi' : Math.PI,
+          'theta': Math.PI,   
+          'position' : new THREE.Vector3(
+            rayon * Math.cos(Math.PI) * Math.sin(3*Math.PI/2), // Attention Y sphère
+            rayon * Math.sin(Math.PI), // Attention Z de la sphère
+            rayon * Math.cos(Math.PI) * Math.cos(3*Math.PI/2) // Attention X de la sphère
+          ),
+          'text': 'couloir prof escalier depuis couloir prof i6',
+          'destination': 'g4_couloir_prof_i6'
+        } 
+      ]
+  },
+];
